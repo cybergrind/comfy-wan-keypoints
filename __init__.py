6df30a5de354fee94783da0b177ca27391e02c0b
Kirill Pinchuk <cybergrind@gmail.com>
2025-08-31 11:44:37 +0300

    initial commmit

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for wan_keypoints."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """comfy-wan-keypoints"""
+__email__ = "cybergrind@gmail.com"
+__version__ = "0.9.0"
+
+from .src.wan_keypoints.nodes import NODE_CLASS_MAPPINGS
+from .src.wan_keypoints.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
